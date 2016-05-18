a3d69723948897e81935775e9a23d3e936920f3d
Bobbie LaFollette <colorwheelie@gmail.com>
2016-05-18 15:42:20 -0400

    Added strings example

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿/**** Fiddle Challenge #! - "Hello World"  ****/
+
+//TODO: With a partner or group, create comments that analyze a basic C# "Hello World".
+
+//REFERENCES: https://msdn.microsoft.com/en-us/library/0d941h9d.aspx
+//https://msdn.microsoft.com/en-us/library/x9afc042.aspx
+//https://msdn.microsoft.com/en-us/library/79b3xss3.aspx
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* - 1 - */
+//Allows you to import prebuilt stuff to access, bring in libraries you think you're going to use.
+using System;
+
+/* - 2 - */
+namespace HelloWorld
+{
+    /* - 3 - */
+    // Creating a class within this namespace.
+    public class Program
+    {
+        /* - 4 - */
+        //The Main function is where the program begins.
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    //1 The word Using is used so that the complete name is not required, as in System.Console.WriteLine()
+    //2 The namespace keyword is used to declare a namespace, which can help control the scope of class and method names in larger programming projects.
+    //3 Classes are declared by using the class keyword.  Because the word public is used, anyone can create objects from this class.
+    //4 A static class can't be instantiated.  You access the members of a static class by using the class name itself.  Static classes are sealed and can't be inherited.
+    //5 Console.WriteLine tells it to putput the string to the console.
+
+}
---
+++ StringReverse/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class Program
+    {
+        //Bronze Challenge - Write a method that determines whether a number is odd or even.
+        int num1 = 2;
+
+        public void OddOrEven1(int num1)
+        {
+            if (num1 % 2 == 0)
+                Console.WriteLine("The number is even.");
+            else
+                Console.WriteLine("The number is odd.");
+            Console.ReadLine();
+        }
+
+        //Silver Challenge - Write a method that can reverse a string ("Hello World" becomes "dlroW olleH")
+        //https://msdn.microsoft.com/en-us/library/ezftk57x(v=vs.110).aspx
+
+        string text = "Hello world";
+
+        public string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();             //Copies each character in a string to a character array.
+            string Reverse = String.Empty;                  //Sets our output string equal to an empty string.
+            for (int i = cArray.Length - 1; i > -1; i--)    //Iterates through the array by starting at the end and working backwards.
+            {
+                Reverse += cArray[i];                       //Adds each character to our new array.
+            }
+            return Reverse;                                 //Returns our new array.
+        }
+
+
+        //--or--
+
+        string s = "Hello world";
+
+        public string Reverse1(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
